6c4bbf0e84ecedf1785170abfc8d1c90ef12d504
F <fmalina@gmail.com>
2015-04-02 21:50:48 +0100

    Add cropping example.

---
+++ crop/crop.py
@@ -0,0 +1,9 @@
+import Image
+
+def crop(left, top, width, height):
+    img = Image.open('example.jpg')
+    box = (left, top, left+width, top+height)
+    img.crop(box).save('cropped.jpg')
+
+if __name__ == '__main__':
+    crop(432, 295, 265, 210)
