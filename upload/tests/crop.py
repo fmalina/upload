c678403d265dd4763e94243631bd95cc2f7ea316
F <fmalina@gmail.com>
2016-06-28 06:52:22 +0100

    Removed PIL cropping example.

--- upload/tests/crop.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-def crop(left, top, width, height):
-    img = Image.open('example.jpg')
-    box = (left, top, left+width, top+height)
-    img.crop(box).save('cropped.jpg')
-
-if __name__ == '__main__':
-    crop(432, 295, 265, 210)
