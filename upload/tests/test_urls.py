b5bcd20251654accd9a56e727c916502256277c1
F <fmalina@gmail.com>
2016-06-30 08:12:59 +0100

    Added simple integration to the core.

--- upload/tests/test_urls.py
+++ upload/tests/test_urls.py
@@ -2,11 +2,9 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
 from upload.tests.test_views import index, album
-from upload.views_post import FilesEditView
 
 urlpatterns = [
     url(r'^$', index),
-    url(r'^post$', FilesEditView.as_view(), name="post"),
     url(r'^(?P<pk>[0-9]+)$', album),
     url(r'^upload/', include('upload.urls')),
 ]
