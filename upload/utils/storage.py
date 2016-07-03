26f97c754960f1b6816cacb4d2fcd62f0df08cac
F. Malina <fmalina@pm.me>
2016-07-03 17:13:59 +0200

    Made it easier to workout the file path.

--- upload/utils/storage.py
+++ upload/utils/storage.py
@@ -1,16 +1,12 @@
 from django.conf import settings
-from upload import app_settings
-
-
-def ext3_shard(uid):
-    """ext3 subfolders limit workaround"""
-    return int(uid) // (32000-2)
 
 
 def img_url(n, uid):
-    folder = settings.MEDIA_URL+'tmp'
+    folder = 'tmp'
     if uid:
-        folder = settings.MEDIA_URL+'%s/%s' % (ext3_shard(uid), uid)
+        # ext3 subfolders limit workaround
+        ext3_shard = int(uid) // (32000-2)
+        folder = '%s/%s' % (ext3_shard, uid)
     if n is not None:
-        return folder + '/%s.jpg' % n
+        return settings.MEDIA_URL + folder + '/%s.jpg' % n
     return ''
