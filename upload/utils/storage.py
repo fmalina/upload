fac2400416ede1ef42e6057fd72d6e35b567c9e4
F. Malina <fmalina@pm.me>
2016-07-03 17:15:31 +0200

    Amend

--- upload/utils/storage.py
+++ upload/utils/storage.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 
 
-def img_url(n, uid):
+def img_url(file_id, uid):
     folder = 'tmp'
     if uid:
         # ext3 subfolders limit workaround
         ext3_shard = int(uid) // (32000-2)
         folder = '%s/%s' % (ext3_shard, uid)
-    if n is not None:
-        return settings.MEDIA_URL + folder + '/%s.jpg' % n
+    if file_id is not None:
+        return settings.MEDIA_URL + folder + '/%s.jpg' % file_id
     return ''
