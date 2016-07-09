21be4ea99eb6acab2bcae982f1beb22ba73581d3
F. Malina <fmalina@pm.me>
2016-07-09 14:26:28 +0200

    PEP8

--- upload/utils/imaging.py
+++ upload/utils/imaging.py
@@ -1,9 +1,10 @@
 from PIL import Image, ImageChops
 
+
 def autocrop(im, fuzz=0.5):
     """Get the border colour from the top left pixel, using getpixel.
     Subtracts a scalar from the differenced image, this is a quick way
-    of saturating all values under 100, 100, 100 to zero to remove any 'wobble'
+    of saturating all values under 100, 100, 100 to zero to remove any fuzz
     resulting from compression.
     """
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
