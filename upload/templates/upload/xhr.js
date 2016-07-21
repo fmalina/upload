de4374d68c62bac2d647198c8c7feaa3661f1848
F. Malina <fmalina@pm.me>
2016-07-21 15:49:17 +0200

    Made clear that we are fixing troublesome path behaviour of easy_thumbnails.

--- upload/templates/upload/xhr.js
+++ upload/templates/upload/xhr.js
@@ -1,1 +1,1 @@
-{% load thumbnail upload_tags %}{% thumbnail path 180x180 crop=crop as t %}{'id':{{ id }},'url':'{{ t.path|absolute_url }}'}
+{% load thumbnail upload_tags %}{% thumbnail path 180x180 crop=crop as t %}{'id':{{ id }},'url':'{{ t.url|fixurl }}'}
