93853b2de0b63b631a5406a88b52929eaab41a44
F <fmalina@gmail.com>
2016-07-28 15:01:00 +0200

    Simplified social media profile photo integration. Leave association to user model up to the dev integrating this.

--- upload/social_profile.py
+++ upload/social_profile.py
@@ -1,9 +1,8 @@
 """Django allauth integration that can download social profile image
 from Google and Facebook and save it on their first collection.
 """
-from allauth.socialaccount.models import SocialAccount
-from upload.models import File, get_collection_model
 from upload.utils.download import get_missing_file
+from upload.models import File
 
 
 FB_API = 'http://graph.facebook.com/'
@@ -14,7 +13,6 @@ def load_profile_image(account):
     """Load social media profile image.
     """
     a = account
-    print(a.user.username.upper())
 
     if a.provider == 'google':
         url = a.extra_data['picture']
@@ -24,16 +22,7 @@ def load_profile_image(account):
     if a.provider == 'facebook':
         url = FB_API + '%s/picture?height=500' % a.extra_data['id']
 
-    col = get_collection_model().objects.filter(user=a.user).first()
-    if col and not col.file_set.filter(alt='me'):
-        f = File(fn='', alt='me', pos=99, col=col)
-        f.save()
-        get_missing_file(f, url)
-        print(f.url())
-        col.save(staff=True)
-    return col
-
-
-def bulk_load():
-    for account in SocialAccount.objects.all():
-        load_profile_image(account)
+    f = File(fn='', alt='me', pos=99, col=None)
+    f.save()
+    get_missing_file(f, url)
+    return f
