cb272d1c7eb09ac5e8edc505c6134d70fdf0ccfa
F <fmalina@gmail.com>
2016-08-30 10:50:44 +0100

    Fixed URL issue with thumbnails.

--- upload/templatetags/upload_tags.py
+++ upload/templatetags/upload_tags.py
@@ -6,4 +6,7 @@ register = Library()
 
 @register.filter
 def fixurl(path):
-    return path.replace(settings.STATIC_ROOT, '/')
+    """Fix common problems with easy_thumbnail generated thumbnail URLs
+    """
+    return path.replace(settings.STATIC_ROOT, '/')\
+               .replace('media/media', 'media')
