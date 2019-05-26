3832f3488f209fec97350f67c2cac7ae7d83c14d
Francis Malina <fmalina@gmail.com>
2019-05-26 10:25:31 +0100

    Updated urls to use path and re_path

--- upload/urls.py
+++ upload/urls.py
@@ -1,31 +1,27 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from upload.views import upload, edit
 from upload.views_post import FilesEditView
 
 urlpatterns = [
     # XHR handler to upload files
-    url(r'^$',
-        upload, name='xhr_up'),
+    path('', upload, name='xhr_up'),
     # XHR handler for files in existing collection
-    url(r'^(?P<pk>[0-9]+)$',
-        upload, name='xhr_up_col'),
+    path('<int:pk>', upload, name='xhr_up_col'),
     # XHR handler for files associated with any object via generic foreign key
-    url(r'^(?P<app_label>[a-z]+)-(?P<model>[a-z]+)/(?P<object_id>[0-9]+)$',
-        upload, name='xhr_up_gfk'),
+    re_path(r'^(?P<app_label>[a-z]+)-(?P<model>[a-z]+)/(?P<object_id>[0-9]+)$',
+            upload, name='xhr_up_gfk'),
 
     # Post collection
-    url(r'^post$',
-        FilesEditView.as_view(), name="upload_col"),
+    path('post', FilesEditView.as_view(), name="upload_col"),
 
     # Edit existing collection
-    url(r'^(?P<pk>[0-9]+)/edit$',
-        FilesEditView.as_view(), name='upload_col_edit'),
+    path('<int:pk>/edit', FilesEditView.as_view(), name='upload_col_edit'),
 
     # Edit files with generic foreign key relation
-    url(r'^(?P<app_label>[a-z]+)-(?P<model>[a-z]+)/(?P<object_id>[0-9]+)/edit$',
-        FilesEditView.as_view(), name='upload_gfk_edit'),
+    re_path(r'^(?P<app_label>[a-z]+)-(?P<model>[a-z]+)/(?P<object_id>[0-9]+)/edit$',
+            FilesEditView.as_view(), name='upload_gfk_edit'),
 
     # Crop and rotate images
-    url(r'^crop/(?P<pk>[0-9]+)$', edit, name='crop'),
-    url(r'^rotate/(?P<angle>(90|270))/(?P<pk>[0-9]+)$', edit, name='rotate'),
+    path('crop/<int:pk>', edit, name='crop'),
+    re_path(r'^rotate/(?P<angle>(90|270))/(?P<pk>[0-9]+)$', edit, name='rotate'),
 ]
